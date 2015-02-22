ee26f480705a00736e45dc6c5fa8eab494b23417
♥CodingFever♥ <78756e61@gmail.com>
2015-02-23 00:25:39 +1030

    Modifying test cases

--- HeapSort/HeapSortAscTest.java
+++ HeapSort/HeapSortAscTest.java
@@ -1,52 +1,66 @@
-//Heap Sort in an ascending order ( an in-place sorting algorithm)
+//Heap Sort in an ascending order (an in-place sorting algorithm)
 //1. Build a max heap from bottom up
 //2. Sort the array  
-//@ Xuna Xu [78756e61@gmail.com]
+//@ Xuna Xu [78756e61@gmail.com] Happy Coding!
 
 import java.util.Arrays;
 
 public class HeapSortAscTest{
+	
     public static void main(String[] args){
+
         //case 1: empty array
-        int[] arr1 = {};
-        HeapSortAsc heapSort1 = new HeapSortAsc(arr1);
-        System.out.println(Arrays.toString(heapSort1.sortAsc()));
-        
+        test(new int[]{}, new int[]{});
+
         //case 2: one element array
-        int[] arr2 = {1};
-        HeapSortAsc heapSort2 = new HeapSortAsc(arr2);
-        System.out.println(Arrays.toString(heapSort2.sortAsc()));
-        
+        test(new int[]{2}, new int[]{2});
+
         //case 3: two element array
-        int[] arr3 = {1,3};
-        HeapSortAsc heapSort3 = new HeapSortAsc(arr3);
-        System.out.println(Arrays.toString(heapSort3.sortAsc()));
+        test(new int[]{1,3}, new int[]{1,3});
         
         //case 4: odd number length n>3 array
-        int[] arr4 = {8,-3,17,1,0};
-        HeapSortAsc heapSort4 = new HeapSortAsc(arr4);
-        System.out.println(Arrays.toString(heapSort4.sortAsc()));
-        
+        test(new int[]{8,-3,17,1,0}, new int[]{-3,0,1,8,17});
+
         //case 5: even number length n>3 array
-        int[] arr5 = {2,96,1,83,23,1,4,-7};
-        HeapSortAsc heapSort5 = new HeapSortAsc(arr5);
-        System.out.println(Arrays.toString(heapSort5.sortAsc()));
+        test(new int[]{2,96,1,83,23,1,4,-7}, new int[]{-7,1,1,2,4,23,83,96});
         
         //case 6: an array with duplicates
-        int[] arr6 = {3,3,3,1,5};
-        HeapSortAsc heapSort6 = new HeapSortAsc(arr6);
-        System.out.println(Arrays.toString(heapSort6.sortAsc()));
+        test(new int[]{3,3,3,1,5}, new int[]{1,3,3,3,5});
     }
+
+	public static void test(int[] original, int[] expected){
+		HeapSortAsc heapSort = new HeapSortAsc();
+		int[] originalCopy = original.clone();  //not ideal due to the in-place sorting alg
+		int[] result = heapSort.sortAsc(original);
+		System.out.println(Arrays.equals(expected, result)+" "+Arrays.toString(originalCopy)+" => "+Arrays.toString(result)); 
+	}
 }
 
 class HeapSortAsc{
     private int[] arr;
     
-    //constructor
-    public HeapSortAsc(int[] arr){
-        this.arr = arr;
+    public HeapSortAsc(){
     }
     
+    //heap sort in an ascending order
+	//swap the first element to the last leaf
+	//run build max heap again, till everything is sorted
+	//O(NLogN) complexity
+    public int[] sortAsc(int[] arr){
+        this.arr = arr;
+		int temp=0;
+
+		for(int i=arr.length; i>0; i--){
+			//buildMaxHeap_Sol1(i); 
+			buildMaxHeap_Sol2(i);
+
+			temp=arr[0];
+			arr[0]=arr[i-1];
+			arr[i-1]=temp;
+		}		
+		return arr;
+    }
+
     //build the max heap tree Sol1 : check all leaves's parents. (a bottom up approach)
 	//parents n/2, 2..n are leaves of a node.
 	//O(N) complexity
@@ -99,21 +113,4 @@ class HeapSortAsc{
         }
     }
 
-    
-    //heap sort in an ascending order
-	//swap the first element to the last leaf
-	//run build max heap again, till everything is sorted
-	//O(NLogN) complexity
-    public int[] sortAsc(){
-		int temp=0;
-		for(int i=arr.length; i>0; i--){
-			//buildMaxHeap_Sol1(i); 
-			buildMaxHeap_Sol2(i);
-
-			temp=arr[0];
-			arr[0]=arr[i-1];
-			arr[i-1]=temp;
-		}		
-		return arr;
-    }
 }
