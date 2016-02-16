b048083d19c7800197c037196e063c1af513a5d0
Xuna <78756e61@gmail.com>
2016-02-16 12:54:51 +1030

    set up relay

---
+++ JS/relay-graphql-pluralsight/webpack.config.js
@@ -0,0 +1,15 @@
+var webconfig = { 
+	entry: "./js/app.js",
+	output: {
+		path: __dirname+ "/public",
+		filename: "bundle.js"
+	},
+	module:{
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader'
+		}]
+	}
+};
+
+module.exports = webconfig;
