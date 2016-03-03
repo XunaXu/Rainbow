7177253e4c10ac426b7a52e427497cf9a2f6fbbd
Xuna Xu <78756e61@gmail.com>
2016-03-03 12:56:20 +1030

    push for relay-plugin, store the snap short before getting ridof flux

--- JS/relay-graphql-pluralsight/server.js
+++ JS/relay-graphql-pluralsight/server.js
@@ -2,6 +2,10 @@ import express from "express"
 import Schema from './data/schema'
 import {MongoClient} from 'mongodb'
 import GraphQLHTTP from 'express-graphql'
+import {graphql} from 'graphql'
+import {introspectionQuery} from 'graphql/utilities'
+import fs from 'fs'
+
 let app = express()
 
 app.use(express.static('public'))
@@ -12,14 +16,24 @@ let MONGO_URL="mongodb://testing_relay_graphql:+testing_relay_graphql+@ds011288.
 
 (async () =>{
 	let db = await MongoClient.connect(MONGO_URL)
+	let schema = Schema(db);
+	//console.log("schema: ", schema);
 
 	app.use('/graphql', GraphQLHTTP({
-		schema: Schema(db),
+		schema,
 		graphiql: true
 	}))
 
 	app.listen(3000, ()=>console.log('listening on port 3000'))
 
+	let json = await graphql(schema, introspectionQuery);
+
+	console.log("json", json);
+
+	fs.writeFile('./data/schema.json', JSON.stringify(json, null, 2), err=>{
+		if(err) throw err;
+		console.log("JSON schema created");
+	})
 })()
 
 /*let db;
