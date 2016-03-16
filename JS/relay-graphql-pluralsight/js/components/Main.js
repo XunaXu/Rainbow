f9f08fc5619b81e4b25e211f7b62222cb3551be4
Xuna Xu <78756e61@gmail.com>
2016-03-16 12:46:10 +1030

    componentes for relay

--- JS/relay-graphql-pluralsight/js/components/Main.js
+++ JS/relay-graphql-pluralsight/js/components/Main.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Relay from 'react-relay'
+import Link from './Link'
 
 class Main extends React.Component {
 
-	static propTypes = { limit: React.PropTypes.number}
-	static defaultProps = {limit: 4}
-
 	render() {
-		let content = this.props.store.links.slice(0,this.props.limit).map(link => {
-			//TODO: make the li as <Link /> Decomponent
-			return <li key={link._id}><a href={link.url}>{link.title}</a></li>;
+		let content = this.props.store.links.map(link => {
+			return <Link key = {link._id} link={link} />;
 		})
 		return (  
 		<div>
@@ -28,8 +25,7 @@ Main = Relay.createContainer(Main, {
 			fragment on Store {
 				links {
 					_id,
-					title,
-					url
+					${Link.getFragment('link')}
 				}
 			}
 		`
